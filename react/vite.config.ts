2f98a788956150b6560a6950f80982c77e7ba7d7
Herman Jensen <herman.jensen@intility.no>
2025-05-15 14:17:11 +0200

    build: add vite-plugin-checker

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,8 +1,18 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      biome: {
+        command: "check",
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
