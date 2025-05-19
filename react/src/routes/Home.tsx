8955f9d8f29bf325f1208f995b5b2c2b57e27e91
Herman Jensen <herman.jensen@intility.no>
2025-05-19 13:13:44 +0200

    feat: add layout and color mode

--- react/src/routes/Home.tsx
+++ react/src/routes/Home.tsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import reactLogo from "~/assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./Home.css";
+import styles from "./Home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className={styles.home}>
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className={`${styles.logo} ${styles.react}`}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={styles.card}>
         <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,9 +29,9 @@ export default function Home() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={styles.readTheDocs}>
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
