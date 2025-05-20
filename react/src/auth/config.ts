c5260d34735a7637184cadf6d70b075667c72a19
Herman Jensen <herman.jensen@intility.no>
2025-05-20 09:40:55 +0200

    fix: use replaceable variables and names

--- react/src/auth/config.ts
+++ react/src/auth/config.ts
@@ -6,10 +6,8 @@ import type { Configuration, RedirectRequest } from "@azure/msal-browser";
  */
 export const msalConfig: Configuration = {
   auth: {
-    clientId: "YOUR_CLIENT_ID",
-    // if multi-tenant, use https://login.microsoftonline.com/common
-    authority:
-      "https://login.microsoftonline.com/9b5ff18e-53c0-45a2-8bc2-9c0c8f60b2c6",
+    clientId: import.meta.env.VITE_ENTRA_CLIENT_ID,
+    authority: import.meta.env.VITE_ENTRA_AUTHORITY,
     redirectUri: window.location.origin,
   },
 };
--- react/src/utils/instrument.ts
+++ react/src/utils/instrument.ts
@@ -7,22 +7,24 @@ import {
   useNavigationType,
 } from "react-router";
 
-// https://docs.sentry.io/platforms/javascript/guides/react/
-Sentry.init({
-  dsn: "SENTRY_DSN",
+if (import.meta.env.VITE_SENTRY_DSN) {
+  // https://docs.sentry.io/platforms/javascript/guides/react/
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
 
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+    // Adds request headers and IP for users, for more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#sendDefaultPii
+    sendDefaultPii: true,
 
-  integrations: [
-    // https://docs.sentry.io/platforms/javascript/guides/react/features/react-router/v7/#usage-with-createbrowserrouter-or-creatememoryrouter
-    Sentry.reactRouterV7BrowserTracingIntegration({
-      useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes,
-    }),
-  ],
-});
+    integrations: [
+      // https://docs.sentry.io/platforms/javascript/guides/react/features/react-router/v7/#usage-with-createbrowserrouter-or-creatememoryrouter
+      Sentry.reactRouterV7BrowserTracingIntegration({
+        useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      }),
+    ],
+  });
+}
