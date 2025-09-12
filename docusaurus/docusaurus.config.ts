995a7f2efe7adaabc50e3c057204a8deb25befa2
Linda Tran <linda.tran@intility.no>
2025-09-12 11:33:45 +0200

    feat: add Bifrost theme plugin

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 const config: Config = {
   future: {
@@ -17,6 +18,8 @@ const config: Config = {
   themeConfig: {
     defaultMode: 'dark',
     prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
       additionalLanguages: ['csharp'],
     },
     navbar: {
@@ -118,14 +121,12 @@ const config: Config = {
           editUrl: 'https://github.com/Intility/templates/tree/main/docusaurus',
           sidebarCollapsible: false,
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
       } satisfies Preset.Options,
     ],
   ],
   plugins: [
     require.resolve('docusaurus-lunr-search'),
+    require.resolve('@intility/docusaurus-plugin-bifrost-theme'),
     [
       '@docusaurus/plugin-content-docs',
       {
