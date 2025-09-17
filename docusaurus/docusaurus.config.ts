eeb46016c273a256ce7f3b392800e9955717950f
Linda Tran <linda.tran@intility.no>
2025-09-17 13:55:47 +0200

    fix: new docusaurus config

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -1,169 +1,165 @@
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
-import { themes as prismThemes } from 'prism-react-renderer';
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
+  title: "Create Intility App",
+  tagline: "Easy to use templates for Intility developers",
+  favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true,
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
     experimental_faster: true,
   },
-  title: 'Create Intility App',
-  tagline: 'Easy to use templates for Intility developers',
-  url: 'https://create.intility.app/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'intility', // Usually your GitHub org/user name.
-  projectName: 'templates', // Usually your repo name.
+
+  // Set the production url of your site here
+  url: "https://create.intility.app/",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "intility", // Usually your GitHub org/user name.
+  projectName: "templates", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: false,
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  plugins: [
+    require.resolve("docusaurus-lunr-search"),
+    require.resolve("@intility/docusaurus-plugin-bifrost-theme"),
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "react",
+        path: "react",
+        routeBasePath: "react",
+        editUrl: "https://github.com/Intility/templates/tree/main/docusaurus",
+        sidebarPath: "./sidebars.ts",
+        sidebarCollapsible: false,
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "dotnet",
+        path: "dotnet",
+        routeBasePath: "dotnet",
+        editUrl: "https://github.com/Intility/templates/tree/main/docusaurus",
+        sidebarPath: "./sidebars.ts",
+        sidebarCollapsible: false,
+      },
+    ],
+  ],
+
   themeConfig: {
-    defaultMode: 'dark',
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-      additionalLanguages: ['csharp'],
-    },
     navbar: {
-      title: 'Create Intility App',
+      title: "Create Intility App",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo192.png',
+        alt: "My Site Logo",
+        src: "img/logo192.png",
       },
       items: [
         {
-          href: '/react',
-          label: 'React',
-          position: 'left',
+          to: "/react",
+          label: "React",
+          position: "left",
         },
         {
-          href: '/dotnet',
-          label: '.NET API',
-          position: 'left',
+          to: "/dotnet",
+          label: ".NET API",
+          position: "left",
         },
         {
-          href: 'https://github.com/Intility/templates',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/Intility/templates",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Bifrost',
-              href: 'https://bifrost.intility.com/',
+              label: "Bifrost",
+              href: "https://bifrost.intility.com/",
             },
             {
-              label: 'React',
-              href: 'https://reactjs.org/',
+              label: "React",
+              href: "https://reactjs.org/",
             },
             {
-              label: 'Vite',
-              href: 'https://vitejs.dev/',
+              label: "Vite",
+              href: "https://vitejs.dev/",
             },
           ],
         },
         {
-          title: 'Channels',
+          title: "Channels",
           items: [
             {
-              label: '#programming',
-              href: 'https://intility.slack.com/archives/CRTT6METE',
+              label: "#programming",
+              href: "https://intility.slack.com/archives/CRTT6METE",
             },
             {
-              label: '#frontend',
-              href: 'https://intility.slack.com/archives/CRVGGS6R1',
-            },
-            {
-              label: 'Backstage',
-              href: 'https://backstage.apps.aa.intility.com/',
+              label: "Backstage",
+              href: "https://backstage.apps.aa.intility.com/",
             },
           ],
         },
         {
-          title: 'Intility Services',
+          title: "Intility Services",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/intility',
+              label: "GitHub",
+              href: "https://github.com/intility",
             },
             {
-              label: 'Sentry',
-              href: 'https://sentry.intility.no/',
+              label: "Sentry",
+              href: "https://sentry.intility.no/",
             },
             {
-              label: 'OpenShift',
-              href: 'https://console-openshift-console.apps.openshift.intility.com/',
+              label: "OpenShift",
+              href: "https://console-openshift-console.apps.openshift.intility.com/",
             },
             {
-              label: 'Azure Portal',
-              href: 'https://portal.azure.com/',
+              label: "Azure Portal",
+              href: "https://portal.azure.com/",
             },
           ],
         },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Intility AS. Built with Docusaurus.`,
     },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ["csharp"],
+    },
   } satisfies Preset.ThemeConfig,
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: undefined,
-          id: 'react',
-          path: 'react',
-          routeBasePath: 'react',
-          // Please change this to your repo.
-          editUrl: 'https://github.com/Intility/templates/tree/main/docusaurus',
-          sidebarCollapsible: false,
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
-  plugins: [
-    require.resolve('docusaurus-lunr-search'),
-    require.resolve('@intility/docusaurus-plugin-bifrost-theme'),
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'dotnet',
-        path: 'dotnet',
-        routeBasePath: 'dotnet',
-        sidebarPath: undefined,
-        editUrl: 'https://github.com/Intility/templates/tree/main/docusaurus',
-        sidebarCollapsible: false,
-
-        /**
-         * Filters any pages on 2nd level if the label starts with underscore (_).
-         * This sidebar plugin is currently not in use and should be refactored into
-         * a proper front matter plugin with property to indicate if article is hidden.
-         * @param {*} param0
-         * @returns
-         */
-        sidebarItemsGenerator: async function ({
-          defaultSidebarItemsGenerator,
-          ...args
-        }) {
-          const sidebarItems = await defaultSidebarItemsGenerator(args);
-          for (let category of sidebarItems) {
-            if (category.items) {
-              category.items = category.items.filter(
-                (i) => !i.label || !i.label.startsWith('_'),
-              );
-            }
-          }
-          return sidebarItems.filter(
-            (c) => !c.label || !c.label.startsWith('_'),
-          );
-        },
-        // ... other options
-      },
-    ],
-  ],
 };
 
 export default config;
